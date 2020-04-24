d00d3c16fe2071a41b59a55a0a0c4c9f750868f6
hhhhh-bobby <64215111+hhhhh-bobby@users.noreply.github.com>
2020-04-24 18:07:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -32,11 +32,11 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "装饰",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        wish_message: "祝五月宝宝生日快乐",
         story: "A MESSAGE FOR YOU",
     }
 };
